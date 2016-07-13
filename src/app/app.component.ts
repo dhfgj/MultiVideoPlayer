e5c0ba459f798887c01ec524996b795aa003846b
Sridhar Mane <Sridhar.Mane@RoswellPark.org>
2016-07-13 14:29:48 -0400

    Added map service to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  OnInit,
+  HostListener 
+} from '@angular/core';
 import { VideoControlsComponent } from './video-controls'
 import { VideoElementsComponent } from './video-elements'
 import { MD_GRID_LIST_DIRECTIVES } from '@angular2-material/grid-list';
@@ -7,45 +12,67 @@ import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
 import { MD_RADIO_DIRECTIVES } from '@angular2-material/radio';
 import { MdUniqueSelectionDispatcher } from '@angular2-material/core/coordination/unique-selection-dispatcher';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+import {MVPService } from './mvp.service';
+
+const KeyboardKeyCodes = {
+  KEY_C: 99,
+  SPACE: 32
+};
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  directives: [VideoControlsComponent, VideoElementsComponent, MD_GRID_LIST_DIRECTIVES, MD_SIDENAV_DIRECTIVES, MdIcon, MD_RADIO_DIRECTIVES, MD_LIST_DIRECTIVES],
+  directives: [
+    VideoControlsComponent,
+    VideoElementsComponent,
+    MD_GRID_LIST_DIRECTIVES,
+    MD_SIDENAV_DIRECTIVES,
+    MdIcon,
+    MD_RADIO_DIRECTIVES,
+    MD_LIST_DIRECTIVES
+  ],
   providers: [MdIconRegistry, MdUniqueSelectionDispatcher],
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
+
+
 export class AppComponent implements OnInit {
   title = 'app works yay!';
+  currentTime = null;
+  mvpService: MVPService;
+  playbackSpeed = 1;
+
+  playbackSpeedOptions = [
+    { value: 1, label: '1x' },
+    { value: 1.5, label: '1.5x' },
+    { value: 2, label: '2x' }
+  ];
+
   @ViewChild('mvpSidenav') sidenav;
   @ViewChild('videoElements') videoElements;
   @ViewChild('videoControls') videoControls;
 
-  currentTime = null;
 
-  playbackSpeed = 1;
+  @HostListener('document:keypress', ['$event'])
+  keyPressHandler(event: KeyboardEvent): void {
+    // this.key = event.key;
+    switch (event.keyCode) {
+      case KeyboardKeyCodes.SPACE:
+        this.mvpService.togglePlayPause();
+        break;
 
-  playbackSpeedOptions = [
-    { value: 1, label: "1x" },
-    { value: 1.5, label: "1.5x" },
-    { value: 2, label: "2x" }
-  ];
-
-  folders =[
-    {
-      name:"Projects",
-      updated:Date.now()
-    },
-    {
-      name:"Later",
-      updated:Date.now()-200
     }
 
-  ];
+  }
+
+  constructor(mvpService: MVPService) {
+    this.mvpService = mvpService;
+  }
+
   ngOnInit() {
-        this.currentTime = this.videoElements.currentTime;
-    }
+    this.currentTime = this.videoElements.currentTime;
+  }
 
   toggleSidenav($event): void {
     console.log($event);
@@ -55,5 +82,5 @@ export class AppComponent implements OnInit {
       this.sidenav.close();
     }
   }
-   
+
 }
--- src/app/mvp.service.ts
+++ src/app/mvp.service.ts
@@ -10,21 +10,22 @@ export class MVPService {
    * Variables
    */
   api: VgAPI;
-  videoSources: Array<Object>;
+  videoSources: Array<any>;
+  firstElement: any;
   sanitizer: DomSanitizationService;
 
 
   isPlaying: Subject<boolean>;
-  isMute: Subject<boolean>;
+  volumeStatus: Subject<any>;
   isMediaReady: Subject<boolean>;
-  currentTime:Subject<number>;
-  totalDuration:Subject<number>;
+  currentTime: Subject<number>;
+  totalDuration: Subject<number>;
 
   constructor(sanitizer: DomSanitizationService) {
     this.sanitizer = sanitizer;
     this.videoSources = [];
     this.isPlaying = new Subject();
-    this.isMute = new Subject();
+    this.volumeStatus = new Subject();
     this.isMediaReady = new Subject();
     this.currentTime = new Subject();
     this.totalDuration = new Subject();
@@ -34,12 +35,22 @@ export class MVPService {
     this.api = api;
   }
 
+  togglePlayPause(): void {
+    typeof (this.firstElement) === 'undefined' ? this.firstElement = this.videoSources[0] : this.firstElement;
+    console.log(this.api.state);
+
+    if (this.api.state[this.firstElement.id] === "pause" || this.api.state === "pause") {
+      this.play();
+    } else {
+      this.pause();
+    }
+  }
   play(): void {
     console.log("MVP Service: Playing");
     this.api.play();
   }
   pause(): void {
-    console.log("MVP Service: Plausing");
+    console.log("MVP Service: Pausing");
     this.api.pause();
   }
   seekTo(seconds: number): void {
@@ -74,11 +85,26 @@ export class MVPService {
         break;
     }
   }
-  mute(): void {
+  mute(id: string): void {
+    this.api.medias[id].elem.volume = 1;
+  }
+  unmute(id: string): void {
+    this.api.medias[id].elem.volume = 0;
+  }
+  toggleMute(id: string): void {
+    var media = this.api.getMediaById(id);
+    console.log(typeof (media.elem.volume));
+
+    if (media.elem.volume === '0' || media.elem.volume === 0) {
+      this.mute(id);
+    } else {
+      this.unmute(id);
+    }
+  }
+  muteAll(): void {
     this.api.$$setAllProperties('volume', 0);
   }
-  unmute(): void {
-    console.log(this.api);
+  unmuteAll(): void {
     this.api.$$setAllProperties('volume', 1);
   }
 
@@ -113,32 +139,43 @@ export class MVPService {
      */
     // this.api.subscriptions.
     console.log(this.api.subscriptions);
+    typeof (this.firstElement) === 'undefined' ? this.firstElement = this.videoSources[0] : this.firstElement;
+
     //Subscribe to play event
-    this.api.subscriptions[0].play.subscribe(event => { 
-      this.isPlaying.next(true) 
+    this.api.subscriptions[this.firstElement.id].play.subscribe(event => {
+      this.isPlaying.next(true)
     });
     //Subscribe to pause event
-    this.api.subscriptions[0].pause.subscribe(event => { 
-      this.isPlaying.next(false) 
+    this.api.subscriptions[this.firstElement.id].pause.subscribe(event => {
+      this.isPlaying.next(false)
     });
     //Subscribe to volume change event
-    this.api.subscriptions[0].volumeChange.subscribe(event => {
-      let volume = event.target.volume;
-      if (volume === 0) {
-        this.isMute.next(true);
-      } else {
-        this.isMute.next(false);
-      }
-      console.log('VolumeChanged', event.target.volume)
+    this.videoSources.forEach(element => {
+      this.api.subscriptions[element.id].volumeChange.subscribe(event => {
+        let volume = event.target.volume;
+        if (volume === 0) {
+          this.volumeStatus.next({
+              id: event.target.id,
+              value:true
+            });
+        } else {
+          this.volumeStatus.next({
+              id: event.target.id,
+              value:false
+            });;
+        }
+        console.log('VolumeChanged',event.target.id, event.target.volume);
+      });
     });
     //Subscribe to timeupdate event
-    this.api.subscriptions[0].timeUpdate.subscribe(event => {
+    this.api.subscriptions[this.firstElement.id].timeUpdate.subscribe(event => {
       this.currentTime.next(event.target.currentTime);
     });
     
+    
 
     //progress
-    this.api.subscriptions[0].progress.subscribe(event => {
+    this.api.subscriptions[this.firstElement.id].progress.subscribe(event => {
       // this.currentTime.next(event.target.currentTime);
       console.log(event);
       this.totalDuration.next(event.target.duration);
@@ -155,7 +192,7 @@ export class MVPService {
       this.videoSources.push({
         src: this.getObjectURL(file),
         type: file.type,
-        id: currentFileIndex
+        id: 'video' + currentFileIndex
       });
 
       console.log('Added File', file.name);
@@ -165,6 +202,10 @@ export class MVPService {
 
     }
 
+  }
+
+  getVideoSources():Array<any>{
+    return this.videoSources;
   }
 
   /** 
